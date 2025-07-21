aa368c177bad9b0a8c35303a1deeec9fccbec27e
Bryann360 <bryannbueno@hotmail.com>
2025-07-20 22:26:41 -0300

    Add FlightFinder SwiftUI app

---
+++ HelloWorld/Views/FlightDetailView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct FlightDetailView: View {
+    let flight: Flight
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(systemName: flight.icon)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .foregroundColor(.accentColor)
+
+            Text(flight.airline)
+                .font(.largeTitle)
+                .padding(.top)
+
+            Text("Saída: \(flight.departure)")
+            Text("Chegada: \(flight.arrival)")
+            Text("Preço: \(flight.price)")
+                .font(.title2)
+                .padding(.top)
+
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Detalhes")
+    }
+}
+
+#Preview {
+    FlightDetailView(flight: Flight(airline: "Swift Air", departure: "08:00", arrival: "10:00", price: "$199", icon: "airplane"))
+}
---
+++ HelloWorld/Views/ResultsView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct ResultsView: View {
+    @ObservedObject var viewModel: FlightSearchViewModel
+
+    var body: some View {
+        NavigationStack {
+            List(viewModel.results) { flight in
+                NavigationLink(destination: FlightDetailView(flight: flight)) {
+                    FlightRowView(flight: flight)
+                }
+            }
+            .navigationTitle("Voos Encontrados")
+        }
+    }
+}
+
+#Preview {
+    ResultsView(viewModel: FlightSearchViewModel())
+}
