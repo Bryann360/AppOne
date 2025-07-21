0f811fdeb29b20578d2a079618878e725b0a27c7
Bryann Fonseca <bryann.fonseca@barte.com>
2025-07-20 21:29:28 -0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Bryann Bueno on 20/07/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloWorldTests/HelloWorldTests.swift
@@ -0,0 +1,16 @@
+//
+//  HelloWorldTests.swift
+//  HelloWorldTests
+//
+//  Created by Bryann Bueno on 20/07/25.
+//
+
+import Testing
+
+struct HelloWorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
